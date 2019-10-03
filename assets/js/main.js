95da4bd33a157d3906cd7224b1bdac4ab37eaeb8
Larry Mayers <larry.mayers@outlook.com>
2019-10-03 09:40:31 -0400

    Updates

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+    // Unique approach to inserting content within the site dynamically.
+    fetch("./assets/regions/nav.html").then(function(response){
+                return response.text(); 
+                }).then(function(string){
+                  //  Load navigation region.
+                  document.querySelector("header").innerHTML = string;
+                }).catch(function(err){
+                    console.log('Fetch error occurred', err);
+                });         
+
+    // Unique approach to inserting content within the site dynamically.   
+    fetch("./assets/regions/footer.html").then(function(response){
+            return response.text();
+        }).then(function(string){
+            // Load footer region.
+            document.querySelector("footer").innerHTML = string;
+        }).catch(function(err){
+            console.log('Fetch error occurred', err);
+        });
