ec289df63a72712ce6344c3c1d2db60c525ef371
Larry Mayers <larry.mayers@outlook.com>
2019-10-07 13:33:49 -0400

    Updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,34 @@
- 
-    // Unique approach to inserting content within the site dynamically.
+window.onload = function() {
+        var aboutLink = document.getElementById('about');
+        var servicesLink = document.getElementById('services');
+  /* 
+    servicesLink.addEventListener('click', function(){
+
+    });
+*/
+    aboutLink.addEventListener('click', function(){
+       // console.log("About Page Rendered!!!");
+        
+       // Creation of the new XHR Object
+       var xhr = new XMLHttpRequest();
+
+       xhr.open('GET', './assets/regions/content/about.html', true);
+       xhr.onload = function(){
+           if(this.status == 200){
+             //  console.log(this.responseText);
+                document.getElementById("content").innerHTML = this.responseText;
+           } else {
+               xhr = null;
+           }
+       }
+
+       xhr.send();
+    });
+
+
+}
+
+ // Unique approach to inserting content within the site dynamically.
     fetch("./assets/regions/nav.html").then(function(response){
                 return response.text(); 
                 }).then(function(string){
