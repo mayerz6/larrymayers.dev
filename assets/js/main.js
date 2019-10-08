6a170804a0adb27da3e7d43070ed6ca1e9148631
Larry Mayers <larry.mayers@outlook.com>
2019-10-08 00:04:32 -0400

    Updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 window.onload = function() {
+
         var aboutLink = document.getElementById('about');
-        var servicesLink = document.getElementById('services');
+        var expertiseLink = document.getElementById('expertise');
   /* 
     servicesLink.addEventListener('click', function(){
 
@@ -12,17 +13,25 @@ window.onload = function() {
        // Creation of the new XHR Object
        var xhr = new XMLHttpRequest();
 
-       xhr.open('GET', './assets/regions/content/about.html', true);
-       xhr.onload = function(){
-           if(this.status == 200){
-             //  console.log(this.responseText);
-                document.getElementById("content").innerHTML = this.responseText;
-           } else {
-               xhr = null;
+       xhr.open('GET', 'assets/regions/content/about.html', true);
+
+       xhr.onreadystatechange = function(){
+           if(this.readyState == 4 && this.status == 200){
+            // console.log(this.responseText);
+           var block = document.getElementById("content");
+       
+          block.innerHTML = this.responseText;
+        
            }
        }
 
+       xhr.onerror = function(){
+           console.log("Data request error...");
+       }
+
        xhr.send();
+
+
     });
 
 
