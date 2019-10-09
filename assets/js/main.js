afa98d16b3e5e4c162f92b6d48007f20786cd2a0
Larry Mayers <larry.mayers@outlook.com>
2019-10-08 23:27:23 -0400

    Updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,43 @@
 window.onload = function() {
 
-        var aboutLink = document.getElementById('about');
-        var expertiseLink = document.getElementById('expertise');
-  /* 
-    servicesLink.addEventListener('click', function(){
+        let aboutLink = document.getElementById('about');
+        let expertLink = document.getElementById('expert');
+  
+    expertLink.addEventListener('click', function(){
+       console.log("Expertise Page Rendered!!!");
+      
+        let xhr = new XMLHttpRequest();
+
+        xhr.open('GET', './assets/regions/content/expertise.html', true);
+
+        xhr.onreadystatechange = function(){
+            if(this.readyState == 4 && this.status == 200){
+                let block = document.getElementById("content");
+                block.innerHTML = this.responseText;
+              //   console.log(this.responseText);
+            }
+        }
+        
+       xhr.onerror = function(){
+           console.log("Data request error...");
+       }
+
+       xhr.send();
 
     });
-*/
+
     aboutLink.addEventListener('click', function(){
        // console.log("About Page Rendered!!!");
         
        // Creation of the new XHR Object
-       var xhr = new XMLHttpRequest();
+       let xhr = new XMLHttpRequest();
 
-       xhr.open('GET', 'assets/regions/content/about.html', true);
+       xhr.open('GET', './assets/regions/content/about.html', true);
 
        xhr.onreadystatechange = function(){
            if(this.readyState == 4 && this.status == 200){
             // console.log(this.responseText);
-           var block = document.getElementById("content");
+           let block = document.getElementById("content");
        
           block.innerHTML = this.responseText;
         
