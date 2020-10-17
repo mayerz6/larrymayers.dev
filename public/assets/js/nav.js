c845a3d0a047dabd1280198dec9843011acd4cb5
Larry Mayers <larry.mayers@outlook.com>
2020-10-17 12:02:43 -0400

    Slight update to site UI design

--- public/assets/js/nav.js
+++ public/assets/js/nav.js
@@ -1,45 +1,87 @@
 
 class Nav{
-    constructor(){
+
+ constructor(){
         
-           this.aboutLink = document.getElementById("about");
+           this.xhr = new XMLHttpRequest();
+        
+            this.aboutLink = document.getElementById("about");
             this.expertLink = document.getElementById("expert");
             this.certLink = document.getElementById("certs");
             this.contactLink = document.getElementById("contact");
-            
-    
-        this.aboutLink.addEventListener("click", ()=>{console.log("About link clicked!")});
-        this.expertLink.addEventListener("click", this.clickExpertHandler);
-        this.certLink.addEventListener("click", this.clickCertHandler);
-        this.contactLink.addEventListener("click", this.clickContactHandler);
+        
+            this.aboutLink.addEventListener("click", this.clickAboutHandler);
+            this.expertLink.addEventListener("click", this.clickExpertHandler);
+            this.certLink.addEventListener("click", this.clickCertHandler);
+            this.contactLink.addEventListener("click", this.clickContactHandler);
+                
+                // Unique approach to inserting content within the site dynamically.
+                fetch("./assets/regions/nav.html").then(function(response){
+                return response.text(); 
+                }).then(function(string){
+                //  Load navigation region.
+                document.querySelector("header").innerHTML = string;
+                }).catch(function(err){
+                    console.log('Fetch error occurred', err);
+                });         
+
+
+                // Unique approach to inserting content within the site dynamically.   
+                fetch("./assets/regions/footer.html").then(function(response){
+                return response.text();
+                }).then(function(string){
+                // Load footer region.
+                document.querySelector("footer").innerHTML = string;
+                }).catch(function(err){
+                console.log('Fetch error occurred', err);
+                });
+
+
+    }      
+   
+   
+
+   clickAboutHandler(){ 
+        console.log(`The ABOUT link was clicked!!!`);  
+        window.location.href = "./about.html";
+
     }
 
-    clickAboutHandler(){ 
-        
-        let xhr = new XMLHttpRequest();
+    clickExpertHandler(){  
+        console.log("The EXPERT link was clicked!");  
+        window.location.href = "./expertise.html";
+    }
+    
+    clickCertHandler(){ 
+         console.log("The CERT link was clicked!");  
+        window.location.href = "./certs.html";    
+    }
+    
+    clickContactHandler(){  
+        console.log("The CONTACT link was clicked!");  
+        window.location.href = "./contact.html";
+    }
+  
+    fetchCerts(){
 
-        xhr.open('GET', './assets/regions/content/certs.html', true);
-
-        xhr.onreadystatechange = function(){
+        this.xhr.open('GET', './assets/regions/content/certs.html', true);
+    
+        this.xhr.onreadystatechange = function(){
             if(this.readyState == 4 && this.status == 200){
               let block = document.getElementById("content");
-               block.innerHTML = this.responseText;
-               console.clear()
-              //   console.log(this.responseText);
+              // block.innerHTML = "<b>Running</b>";
+               //console.clear()
+                 console.log(this.responseText);
             }
         }
-
-        xhr.onerror = function(){
+    
+        this.xhr.onerror = function(){
             console.log("Data request error...");
         }
- 
-        xhr.send();
-      
-      }
-    clickExpertHandler(){  console.log("The EXPERT link was clicked!");  }
-    clickCertHandler(){  console.log("The CERT link was clicked!");  }
-    clickContactHandler(){  console.log("The CONTACT link was clicked!");  }
     
+        this.xhr.send();
+    }
+
 }
 
 new Nav();
